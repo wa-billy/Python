ebbdfe0e9286905f64d41b7a6aeab7329f4302ea
billy <akwa2562@gmail.com>
2025-12-03 08:51:04 +0800

    successfull

--- AIM_Trainer/aim.py
+++ AIM_Trainer/aim.py
@@ -13,6 +13,9 @@ TARGET_EVENT = pygame.USEREVENT
 TARGET_PADDING = 30
 
 BG_COLOR = (0, 25, 40)
+LIVES = 3
+TOP_BAR_HEIGHT = 50
+LABEL_FONT = pygame.font.SysFont("comicsans", 24)
 
 class Target:
     MAX_SIZE = 30
@@ -52,10 +55,56 @@ def draw(win, targets):
     for target in targets:
         target.draw(win)
 
+def format_time(secs):
+    milli = math.floor(int(secs * 1000 % 1000) / 100)
+    seconds = int(round(secs % 60, 1))
+    minutes = int(secs // 60)
+
+    return f"{minutes:02d}:{seconds:02d}.{milli:02d}"
+
+def draw_top_bar(win, elapsed_time, targets_pressed, misses):
+    pygame.draw.rect(win, "grey", (0, 0, WIDTH, TOP_BAR_HEIGHT))
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
+    speed = round(targets_pressed / elapsed_time, 1)
+    speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
+    
+    hits_label = LABEL_FONT.render(f"Hits: {targets_pressed}", 1, "black")
+    lives_label = LABEL_FONT.render(f"Lives: {LIVES - misses}", 1, "black")
+
+    win.blit(time_label, (5, 5))
+    win.blit(speed_label, (200, 5))
+    win.blit(hits_label, (450, 5))
+    win.blit(lives_label, (600, 5))
+
+def end_screen(win, elapsed_time, targets_pressed, clicks):
+    win.fill(BG_COLOR)
+
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "white")
+
+    speed = round(targets_pressed / elapsed_time, 1)
+    speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "white")
+    
+    hits_label = LABEL_FONT.render(f"Hits: {targets_pressed}", 1, "white")
+    
+    accuracy = round(targets_pressed / clicks * 100, 1)
+    accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}%", 1, "white")
+
+    win.blit(time_label, (get_middle(time_label), 100))
+    win.blit(speed_label, (get_middle(speed_label), 200))
+    win.blit(hits_label, (get_middle(hits_label), 300))
+    win.blit(accuracy_label, (get_middle(accuracy_label), 400))
+
     pygame.display.update()
 
-def draw_top_bar(win, elopsed_time, targets_pressed, misses):
-    pass
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                quit()
+
+def get_middle(surface):
+    return WIDTH / 2 - surface.get_width()/2
 
 def main():
     run = True
@@ -73,6 +122,7 @@ def main():
         clock.tick(60)
         click = False
         mouse_pos = pygame.mouse.get_pos()
+        elapsed_time = time.time() - start_time
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -81,7 +131,7 @@ def main():
 
             if event.type == TARGET_EVENT:
                 x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
-                y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING + TOP_BAR_HEIGHT, HEIGHT - TARGET_PADDING)
                 target = Target(x, y)
                 targets.append(target)
 
@@ -100,7 +150,12 @@ def main():
                 targets.remove(target)
                 target_pressed += 1
 
+        if misses == LIVES:
+            end_screen(WINDOW, elapsed_time, target_pressed, clicks)
+
         draw(WINDOW, targets)
+        draw_top_bar(WINDOW, elapsed_time, target_pressed, misses)
+        pygame.display.update()
 
     pygame.quit()
 
